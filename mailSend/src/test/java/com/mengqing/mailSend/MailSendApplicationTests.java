6c8af0a4b12f9dda592c82cb20fca983a0980724
QianMengqing <Kayla@MengqingdeMacBook-Pro.local>
2019-03-20 14:47:04 +0800

    init

---
+++ mailSend/src/test/java/com/mengqing/mailSend/MailSendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mengqing.mailSend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MailSendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
